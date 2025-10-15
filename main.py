13469612b25351943a09fdd88d1e001297f88e3d
Smit Dangar <smityadav011@gmail.com>
2025-10-15 17:13:53 +0530

    This is a common commit message prefix indicating a new feature or addition.

---
+++ main.py
@@ -0,0 +1,18 @@
+from langchain_ollama import OllamaLLM
+
+from langchain_core.prompts import ChatPromptTemplate 
+
+model = OllamaLLM(model="gemma3:4b")
+
+template = """
+anime.
+
+here are some relevant reviews: {reviews}
+
+here is the question to answer: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+result = chain.invoke({"reviews": [], "question": "magi anime"})
+print(result)
